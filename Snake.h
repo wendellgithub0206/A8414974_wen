f47639f08855a328d4667471a3846795434d02eb
wendellgithub0206 <61726383+wendellgithub0206@users.noreply.github.com>
2020-06-17 11:05:52 +0800

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Snake {
+public:
+	Snake(vector<int>, vector<int>);
+	void setPos(vector<int>);
+	vector<int> getPos();
+	void setDir(vector<int>);
+	vector<int> getDir();
+	void move();
+private:
+	vector<int> pos;
+	vector<int> dir;
+};
 
