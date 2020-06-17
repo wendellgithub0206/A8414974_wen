57e3b698ee3c0872259396e65b295a74f48c5bc5
wendellgithub0206 <61726383+wendellgithub0206@users.noreply.github.com>
2020-06-17 11:06:43 +0800

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,1 +1,34 @@
+#include "Snake.h"
+
+Snake::Snake(vector<int> pos, vector<int> dir)
+{
+	this->pos = pos;
+	this->dir = dir;
+}
+
+void Snake::setPos(vector<int> pos)
+{
+	this->pos = pos;
+}
+
+vector<int> Snake::getPos()
+{
+	return this->pos;
+}
+
+void Snake::setDir(vector<int> dir)
+{
+	this->dir = dir;
+}
+
+vector<int> Snake::getDir()
+{
+	return this->dir;
+}
+
+void Snake::move()
+{
+	this->pos[0] += this->dir[0];
+	this->pos[1] += this->dir[1];
+}
 
